f2cc3708ad18bf24ec1985abc0593c14a7e10dca
socartagena02 <sofiacarcastro@gmail.com>
2026-05-03 10:49:04 -0400

    core: se agrego la configuración de un email

--- core/settings.py
+++ core/settings.py
@@ -116,6 +116,14 @@ CSRF_TRUSTED_ORIGINS = [
     "https://z-stars-ai.onrender.com"
 ]
 
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.outlook.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', True)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
