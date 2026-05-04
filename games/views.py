7faaac39cb2ccb8ec205339c66f4dfce325fce8d
socartagena02 <sofiacarcastro@gmail.com>
2026-05-04 17:59:50 -0400

    fix: resend

--- games/views.py
+++ games/views.py
@@ -17,9 +17,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.conf import settings
 import resend
 
 BASE_DIR = Path(__file__).resolve().parent.parent
